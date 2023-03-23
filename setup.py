6a009e69efb5fe67fd0297778c73002a9f8a9a5a
Alex Telon <alex.telon@gmail.com>
2023-03-23 22:49:02 +0100

    Renamed generator.py -> main.py
    I wonder if I should play around with enforcement of a task related to
    each commit. But I guess that is really something external to what I am
    doing here.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'taskgit=taskgit.generator:main',
+            'taskgit=taskgit.main:main',
         ],
     },
     install_requires=[
