332c853de1b9312114d716864d80a5c81b30b5ea
Alex Telon <alex.telon@gmail.com>
2023-03-24 22:23:57 +0100

    Refactored main

--- taskgit/main.py
+++ taskgit/main.py
@@ -7,32 +7,22 @@ import webbrowser
 
 from .gen import generate_board_html
 
+def init():
+  # create a example task with named parameters
+  example_tasks = [
+      {'id': 1, 'title':'Simple example' },
+      {
+      'id': 2,
+      'title': 'Larger example',
+      'description': 'Description here',
+      'assignee': 'Alex Telon',
+      'column': 'ongoing',
+      }
+  ]
+  with open('tasks.toml', 'w') as f:
+      toml.dump({'task': example_tasks}, f)
 
-def main():
-  parser = argparse.ArgumentParser(prog='taskgit', description='A tool for managing tasks with git')
-  subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
-
-  subparsers.add_parser('init', help='Create a new tasks.toml file')
-  subparsers.add_parser('add',  help='Not implemented yet')
-
-  args = parser.parse_args()
-
-  if args.command == 'init':
-    # create a example task with named parameters
-    example_tasks = [
-       {'id': 1, 'title':'Simple example' },
-       {
-       'id': 2,
-       'title': 'Larger example',
-       'description': 'Description here',
-       'assignee': 'Alex Telon',
-       'column': 'ongoing',
-       }
-    ]
-    with open('tasks.toml', 'w') as f:
-        toml.dump({'task': example_tasks}, f)
-    quit('Created tasks.toml file.')
-
+def create_webpage():
   # Create a .site directory.
   os.makedirs('.site', exist_ok=True)
 
@@ -54,7 +44,24 @@ def main():
   pkg_path = os.path.dirname(os.path.abspath(__file__))
   shutil.copy(os.path.join(pkg_path, 'style.css'), '.site/style.css')
 
-  webbrowser.open('file://' + os.path.realpath('.site/index.html'))
+def open_webpage():
+   webbrowser.open('file://' + os.path.realpath('.site/index.html'))
+
+def main():
+  parser = argparse.ArgumentParser(prog='taskgit', description='A tool for managing tasks with git')
+  subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
+
+  subparsers.add_parser('init', help='Create a new tasks.toml file')
+  subparsers.add_parser('add',  help='Not implemented yet')
+
+  args = parser.parse_args()
+
+  if args.command == 'init':
+   init()
+   quit('Created tasks.toml file.')
+
+  create_webpage()
+  open_webpage()
 
 
 if __name__ == '__main__':
