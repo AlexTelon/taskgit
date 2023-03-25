54f7e04046eae67f83de43fbf2c2582d78c25184
Alex Telon <alex.telon@gmail.com>
2023-03-25 22:12:28 +0100

    Refactoring gen.py
    Not done yet.

--- taskgit/gen.py
+++ taskgit/gen.py
@@ -24,15 +24,22 @@ class Task:
         </div>
         """
 
-
-def generate_board_html(tasks_data: str) -> str:
-    """Generate the HTML for the board from the tasks data and returns a html string."""
+def parse_tasks(tasks_data: str) -> list[Task]:
     if "task" not in tasks_data:
         raise Exception("No tasks found in tasks.toml.")
 
     # Create a list of Task instances from the tasks data
-    tasks = [Task(**task_dict) for task_dict in tasks_data["task"]]
+    return [Task(**task_dict) for task_dict in tasks_data["task"]]
 
+def parse_meta(tasks_data: str) -> dict:
+    # Get the column order. If not specified, use the default order.
+    meta = tasks_data.get("meta", {})
+    column_order = meta.get("order", ["todo", "ongoing", "done"])
+    hidden_columns = meta.get("hidden", [])
+
+    return {"order": column_order, "hidden": hidden_columns}
+
+def validate_ids(tasks: list[Task]):
     # check tasks.id is unique.
     if any(
         duplicates := [
@@ -40,11 +47,11 @@ def generate_board_html(tasks_data: str) -> str:
         ]
     ):
         raise Exception(f"Duplicate task ids: {duplicates}")
+    return tasks
 
-    # Get the column order. If not specified, use the default order.
-    meta = tasks_data.get("meta", {})
-    column_order = meta.get("order", ["todo", "ongoing", "done"])
-    hidden_columns = meta.get("hidden", [])
+def create_columns(tasks: list[Task], meta: dict) -> dict[str, list]:
+    hidden_columns = meta['hidden']
+    column_order = meta['order']
 
     # Get all unique columns from the tasks to a dict
     columns = {task.column: [] for task in tasks if task.column not in hidden_columns}
@@ -61,7 +68,9 @@ def generate_board_html(tasks_data: str) -> str:
         raise Exception(
             f"columns {list(missing_columns)} used but missing in column_order:\n" + hint
         )
+    return columns
 
+def sort_tasks(tasks: list[Task], columns: dict[str, list]) -> dict[str, list[Task]]:
     for col in columns:
         _tasks = [task for task in tasks if task.column == col]
         if col == "done":
@@ -70,6 +79,15 @@ def generate_board_html(tasks_data: str) -> str:
         else:
             # Sort the done tasks by date
             columns[col] = sorted(_tasks, key=lambda task: task.done_date or "", reverse=True)
+      
+
+def generate_board_html(tasks_data: str) -> str:
+    """Generate the HTML for the board from the tasks data and returns a html string."""
+    tasks = parse_tasks(tasks_data)
+    meta = parse_meta(tasks_data)
+    tasks = validate_ids(tasks)
+    columns = create_columns(tasks, meta)
+    sort_tasks(tasks, columns)
 
     # Convert the tasks to HTML
     columns = {col: "".join([task.to_html() for task in tasks]) for col, tasks in columns.items()}
@@ -86,7 +104,7 @@ def generate_board_html(tasks_data: str) -> str:
       </div>
       """
 
-    column_html = "".join([generate_column_html(name) for name in column_order])
+    column_html = "".join([generate_column_html(name) for name in meta['order']])
 
     board_html = f"""
   <!DOCTYPE html>
