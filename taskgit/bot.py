b0e1631eefafb94bc32b93cc34934b06092fab7f
Alex Telon <alex.telon@gmail.com>
2023-03-26 13:30:22 +0200

    Giving bot the full tasks.toml context
    Not very efficient as the total number of prompt tokens with the current
    tasks.toml is 1321. So 0.02642$ for reading the prompt with current
    prices for davinci.

--- taskgit/bot.py
+++ taskgit/bot.py
@@ -7,21 +7,7 @@ PROBLEM_DOMAIN_DESCRIPTION = """\
 You are a bot that adds/deletes/updates tasks in a tasks.toml file.
 The file is used to generate a task board for a project.
 
-[meta]
-order = ['todo', 'ongoing', 'done']
-hidden = ['backlog']
-
-[[task]]
-# Id should be unique
-id = 9
-# Title is mandatory
-title = "Title of the task"
-description = "Description of the task here"
-assignee = "author name here"
-# The columns should be one of the ones defined in the meta section
-# column is mandatory
-column = "backlog
-
+{}
 The comments above are for your benefit, dont generate comments in your response.
 
 You may be asked to update existing tasks or add new ones. Provide correct toml for what the user requests.
@@ -52,7 +38,9 @@ response:
 """
 
 def ask(prompt):
-    prompt = PROBLEM_DOMAIN_DESCRIPTION.format(prompt)
+    with open("tasks.toml", "r") as f:
+        toml_content = f.read()
+    prompt = PROBLEM_DOMAIN_DESCRIPTION.format(toml_content, prompt)
 
     # TODO try out text-davinci-edit-001 for edit tasks
     # TODO how to handle removal of tasks?
