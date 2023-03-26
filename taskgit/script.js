792674b6c271171ed29b4ae2b7db820d6c3e4aed
Alex Telon <alex.telon@gmail.com>
2023-03-26 22:31:40 +0200

    Can now filter on assignee
    It does not compose together with filtering on label. Only the latest
    one applies for now.

--- taskgit/script.js
+++ taskgit/script.js
@@ -1,38 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var labels = document.querySelectorAll('.label');
-  labels.forEach(function(label) {
-    label.addEventListener('click', function(event) {
-      filterTasksByLabel(event.target.textContent);
+    var labels = document.querySelectorAll('.label');
+    labels.forEach(function(label) {
+      label.addEventListener('click', function(event) {
+        filterTasksByLabel(event.target.textContent);
+      });
+    });
+  
+    var assignees = document.querySelectorAll('.assignee');
+    assignees.forEach(function(assignee) {
+      assignee.addEventListener('click', function(event) {
+        filterTasksByAssignee(event.target.textContent.replace('Assignee: ', ''));
+      });
+    });
+  
+    var clearFilterButton = document.getElementById("clear-filter");
+    clearFilterButton.addEventListener("click", function () {
+      clearFilter();
     });
   });
-
-  var clearFilterButton = document.getElementById("clear-filter");
-  clearFilterButton.addEventListener("click", function () {
+  
+  function filterTasksByLabel(labelText) {
     var cards = document.querySelectorAll(".card");
-    cards.forEach(function (card) {
-      card.style.display = "block";
-    });
-    clearFilterButton.style.display = "none";
-  });
-});
-
-function filterTasksByLabel(labelText) {
-  var cards = document.querySelectorAll(".card");
-  var clearFilterButton = document.getElementById("clear-filter");
-  clearFilterButton.style.display = "block";
-  var cards = document.querySelectorAll('.card');
-  cards.forEach(function(card) {
-    var cardLabels = card.querySelectorAll('.label');
-    var hasLabel = false;
-    cardLabels.forEach(function(cardLabel) {
-      if (cardLabel.textContent === labelText) {
-        hasLabel = true;
+    var clearFilterButton = document.getElementById("clear-filter");
+    clearFilterButton.style.display = "block";
+    cards.forEach(function(card) {
+      var cardLabels = card.querySelectorAll('.label');
+      var hasLabel = false;
+      cardLabels.forEach(function(cardLabel) {
+        if (cardLabel.textContent === labelText) {
+          hasLabel = true;
+        }
+      });
+      if (hasLabel) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
       }
     });
-    if (hasLabel) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
+}
+
+function filterTasksByAssignee(assigneeText) {
+    var cards = document.querySelectorAll(".card");
+    var clearFilterButton = document.getElementById("clear-filter");
+    clearFilterButton.style.display = "block";
+    cards.forEach(function(card) {
+        var cardAssignee = card.querySelector('.assignee');
+        if (cardAssignee.textContent.replace('Assignee: ', '') === assigneeText) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
+
+function clearFilter() {
+    var cards = document.querySelectorAll(".card");
+    cards.forEach(function (card) {
+        card.style.display = "block";
+    });
+
+    var clearFilterButton = document.getElementById("clear-filter");
+    clearFilterButton.style.display = "none";
 }
