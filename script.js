f669a171935e69e957dbcc7485164ca78b4c71a9
AlexTelon <AlexTelon@users.noreply.github.com>
2023-04-01 21:24:12 +0000

    deploy: f7be3146486a2473b2c0999d8d9aaf8abe26499d

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function generateColor(type) {
+  const hash = Array.from(type).reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  const hue = hash % 360;
+  const saturation = 80;
+  const lightness = 50;
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+}
+// Give each label a unique color.
+const labels = Array.from(document.querySelectorAll('.label')).sort((a, b) => a.textContent.localeCompare(b.textContent));
+for (const label of labels) {
+  label.style.backgroundColor = generateColor(label.textContent);
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var labels = document.querySelectorAll('.label');
     labels.forEach(function(label) {
